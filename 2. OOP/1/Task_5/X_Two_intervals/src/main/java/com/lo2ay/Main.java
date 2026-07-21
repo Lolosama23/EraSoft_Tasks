c6d16fa535ef6a3764de310d4c0c9c43524307ff
Lolosama23 <125826435+Lolosama23@users.noreply.github.com>
2026-07-21 14:18:09 +0300

    Update Main.java

--- 2. OOP/1/Task_5/X_Two_intervals/src/main/java/com/lo2ay/Main.java
+++ 2. OOP/1/Task_5/X_Two_intervals/src/main/java/com/lo2ay/Main.java
@@ -1,3 +1,37 @@
 package com.lo2ay;
-
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        long l1 = input.nextLong();
+        long r1 = input.nextLong();
+        long l2 = input.nextLong();
+        long r2 = input.nextLong();
+        Interval interval = new Interval(l1,r1,l2,r2);
+        interval.getInterval();
+    }
+    static class Interval
+    {
+        private long l1, r1,l2,r2;
+        public Interval(long l1,long r1,long l2,long r2)
+        {
+            this.l1=l1;
+            this.r1=r1;
+            this.l2=l2;
+            this.r2=r2;
+        }
+        public void getInterval()
+        {
+            long start=Math.max(l1,l2);
+            long end=Math.min(r1,r2);
+ 
+            if (start>end)
+ 
+                System.out.println("-1");
+            else
+                System.out.println(start + " " + end);
+        }
+    }
+}
 
